b52aeefd5be45b48bc2b4383b1d1841a76131c9d
Dexter Fitch <fitch.dexter@gmail.com>
2020-05-27 17:30:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class App < Sinatra::Base
 
     post '/teams' do
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+      members = params[:team][:members]
+      @superheroes = members.collect do |hero|
+        SuperHero.new(name: hero[:name], power: hero[:power], bio: hero[:bio])
+      end
       erb :team
     end
 
